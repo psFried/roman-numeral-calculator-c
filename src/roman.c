16dd491a4991d1dd384f15ca6fefc706e43539bf
pfried <philipsfried@gmail.com>
2016-08-09 13:03:07 -0400

    basic conversions from roman to arabic

--- src/roman.c
+++ src/roman.c
@@ -9,6 +9,11 @@ typedef struct {
 } RomanToInt;
 
 static const RomanToInt ROMAN_NUMERAL_VALUES[] = {
+    {1000, 'M'},
+    {500, 'D'},
+    {100, 'C'},
+    {50, 'L'},
+    {10, 'X'},
     {5, 'V'},
     {1, 'I'}
 };
--- test/roman-to-int-tests.c
+++ test/roman-to-int-tests.c
@@ -17,11 +17,47 @@ START_TEST(V_is_converted_to_5) {
 }
 END_TEST
 
+START_TEST(X_is_converted_to_10) {
+    ck_assert_int_eq(10, roman_to_int("X"));
+}
+END_TEST
+
+START_TEST(L_is_converted_to_50) {
+    ck_assert_int_eq(50, roman_to_int("L"));
+}
+END_TEST
+
+START_TEST(C_is_converted_to_100) {
+    ck_assert_int_eq(100, roman_to_int("C"));
+}
+END_TEST
+
+START_TEST(D_is_converted_to_500) {
+    ck_assert_int_eq(500, roman_to_int("D"));
+}
+END_TEST
+
+START_TEST(M_is_converted_to_1000) {
+    ck_assert_int_eq(1000, roman_to_int("M"));
+}
+END_TEST
+
+START_TEST(MDCLXVI_is_converted_to_1666) {
+    ck_assert_int_eq(1666, roman_to_int("MDCLXVI"));
+}
+END_TEST
+
 TCase* create_roman_to_int_tests() {
     TCase* tests = tcase_create("Roman-To-Int-Tests");
     tcase_add_test(tests, I_is_converted_to_1);
     tcase_add_test(tests, II_is_converted_to_2);
     tcase_add_test(tests, V_is_converted_to_5);
+    tcase_add_test(tests, X_is_converted_to_10);
+    tcase_add_test(tests, L_is_converted_to_50);
+    tcase_add_test(tests, C_is_converted_to_100);
+    tcase_add_test(tests, D_is_converted_to_500);
+    tcase_add_test(tests, M_is_converted_to_1000);
+    tcase_add_test(tests, MDCLXVI_is_converted_to_1666);
 
     return tests;
 }
